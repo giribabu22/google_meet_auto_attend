45617062ec62f89e7d10d1c9a3947b98efea4b93
giribabu <102803078+giribabu22@users.noreply.github.com>
2022-11-17 14:49:06 +0530

    Delete cod.js

--- cod.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://www.geeksforgeeks.org/');
-
-    await browser.close();
-})();
